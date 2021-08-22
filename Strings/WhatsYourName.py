5eb5af4e0bd46d11f7f1f47bc150d4bc18761f41
Sezgin İldeş <47388886+sezginildes@users.noreply.github.com>
2021-08-22 15:46:44 +0300

    new

--- Strings/WhatsYourName.py
+++ Strings/WhatsYourName.py
@@ -1,16 +1,14 @@
-"""
-Title     : Whats Your Name?
-Subdomain : Introduction
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
-Problem   : https://www.hackerrank.com/challenges/whats-your-name/problem
-"""
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
 
-
-def print_full_name(a, b):
-    print("Hello {} {}! You just delved into python.".format(a, b))
-
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello {} {}! You just delved into python.".format(first,last))
 
 if __name__ == '__main__':
     first_name = input()
